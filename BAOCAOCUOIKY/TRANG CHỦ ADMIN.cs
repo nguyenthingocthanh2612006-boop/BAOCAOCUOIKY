986c8e0611222bd4d8316e756ec6a0bad857d923
loi0907772523@gmail.com <loi0907772523@gmail.com>
2026-08-27 18:55:15 +0700

    Hoan thanh giao dien trang chủ admin vs tuyến xe

---
+++ BAOCAOCUOIKY/TRANG CHỦ ADMIN.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BAOCAOCUOIKY
+{
+    public partial class FrmTrangChuAdmin : Form
+    {
+        public FrmTrangChuAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void TRANG_CHỦ_ADMIN_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlMain_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2HtmlLabel15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2HtmlLabel19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
